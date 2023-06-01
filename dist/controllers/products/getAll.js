89111a15fbcd775310191d5906f80617a6fe036e
Kartavchenko <stranger1301078@gmail.com>
2023-06-01 18:58:41 +0200

    deleted script build

--- dist/controllers/products/getAll.js
+++ dist/controllers/products/getAll.js
@@ -6,11 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getAll = void 0;
 const productModel_1 = __importDefault(require("../../models/productModel"));
 const getAll = async (req, res) => {
-    const { page = 1, pageLimit = 10 } = req.query;
+    const { page = 1, pageLimit = 10, query } = req.query;
     const totalProducts = await productModel_1.default.countDocuments();
     const totalPages = Math.ceil(totalProducts / Number(pageLimit));
     // Get products with query params
-    const getProducts = await productModel_1.default.find()
+    const getProducts = await productModel_1.default.find({
+        $or: [
+            { name: { $regex: String(query), $options: "i" } },
+            { description: { $regex: String(query), $options: "i" } },
+        ],
+    })
         .limit(Number(pageLimit))
         .skip((Number(page) - 1) * Number(pageLimit));
     // Get object with pagination info and products
