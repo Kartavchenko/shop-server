be16b0c4c95996835687709375df79755abfc53d
Kartavchenko <stranger1301078@gmail.com>
2023-06-08 21:38:29 +0200

    add index.ts to wishlist

--- src/routes/api/wishlistRouts.ts
+++ src/routes/api/wishlistRouts.ts
@@ -2,9 +2,7 @@ import express from "express";
 import validateBody from "../../middlewares/validateBody";
 import { ctrlWrapper } from "../../helpers";
 import { addToWishlistSchema } from "../../Schemas";
-import { getWishlist } from "../../controllers/wishlist/getWishlist";
-import { addToWishlist } from "../../controllers/wishlist/addToWishlist";
-import { deleteFromWishlist } from "../../controllers/wishlist/deleteFromWishlist";
+import { addToWishlist, getWishlist, deleteFromWishlist } from "../../controllers/wishlist";
 
 const router = express.Router();
 
