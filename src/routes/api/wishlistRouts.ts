ea7023945dc66472451885ac953de1aca91d3f0e
Kartavchenko <stranger1301078@gmail.com>
2023-06-12 11:04:08 +0200

    commit wishlist

--- src/routes/api/wishlistRouts.ts
+++ src/routes/api/wishlistRouts.ts
@@ -1,15 +1,15 @@
-import express from "express";
-import validateBody from "../../middlewares/validateBody";
-import { ctrlWrapper } from "../../helpers";
-import { addToWishlistSchema } from "../../Schemas";
-import { addToWishlist, getWishlist, deleteFromWishlist } from "../../../src/controllers/wishList/index";
+// import express from "express";
+// import validateBody from "../../middlewares/validateBody";
+// import { ctrlWrapper } from "../../helpers";
+// import { addToWishlistSchema } from "../../Schemas";
+// import { addToWishlist, getWishlist, deleteFromWishlist } from "../../controllers/wishList";
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get("/:userId", ctrlWrapper(getWishlist));
+// router.get("/:userId", ctrlWrapper(getWishlist));
 
-router.post("/", validateBody(addToWishlistSchema), ctrlWrapper(addToWishlist));
+// router.post("/", validateBody(addToWishlistSchema), ctrlWrapper(addToWishlist));
 
-router.delete("/:userId/:itemId", ctrlWrapper(deleteFromWishlist));
+// router.delete("/:userId/:itemId", ctrlWrapper(deleteFromWishlist));
 
-export default router;
+// export default router;
